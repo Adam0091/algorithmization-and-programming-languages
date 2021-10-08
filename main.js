92b6051922281359cdd6df1d2bd42fd2c41b40ce
Adam0091 <55671230+Adam0091@users.noreply.github.com>
2021-10-08 14:41:32 +0400

    add: WINDOW (main.js)

--- main.js
+++ main.js
@@ -10,4 +10,24 @@ window.requestAnimFrame = (function () {
 })();
 
 window.onload = function () {
+    const WINDOW = {
+        LEFT: -5,
+        BOTTOM: -5,
+        WIDTH: 10,
+        HEIGHT: 10,
+        P1: new Point (-5, 5, -30),// левый верхний угол
+        P2: new Point(-5, -5, -30),// левый нижний угол
+        P3: new Point (5, -5, -30),//правый нижний угол
+        CENTER: new Point(0, 0, 0),
+        CAMERA: new Point(0, 0, -200)
+    }
+
+    let canPrint = {
+        points: false,
+        edges: false,
+        polygons: true
+    };
+
+    const ZOOM_OUT = 1.1;
+    const ZOOM_IN = 0.9;
 }
